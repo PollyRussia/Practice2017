6505ea03e9e41ca7ce11885e8c29a8c69cc177a6
Polina <paspasskaya@edu.hse.ru>
2017-07-28 20:57:27 +0500

    Add Task 3 (inside project Task 1)

---
+++ Task1/Task 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double x = Double.Parse(input());
+            double y = Double.Parse(input());
+            if (IsPointInSquare(x, y))
+                print("YES");
+            else
+                print("NO");
+            input();
+        }
+        static bool IsPointInSquare(double x1, double y1)
+        {
+            return (Math.Abs(y1) <= -Math.Abs(x1) + 1);
+        }
+        static void print(string s)
+        {
+            Console.WriteLine(s);
+        }
+        static void p()
+        {
+            Console.WriteLine();
+        }
+        static string input()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
